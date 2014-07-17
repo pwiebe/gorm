6d6d2e114a89b3d03a9ec9668065c150991a48ab
Phil Wiebe <phil@cloud.com>
2014-07-17 10:58:23 -0400

    first working arrays processing; NOT generic! Need to abstract into dialect.

--- callback_query.go
+++ callback_query.go
@@ -4,6 +4,8 @@ import (
 	"reflect"
 	"strings"
 	"time"
+
+	"github.com/pub-burrito/pq/arrays"
 )
 
 func Query(scope *Scope) {
@@ -51,10 +53,17 @@ func Query(scope *Scope) {
 
 			columns, _ := rows.Columns()
 			var values []interface{}
+			slices := make(map[reflect.Value]*interface{})
 			for _, value := range columns {
 				field := elem.FieldByName(snakeToUpperCamel(strings.ToLower(value)))
 				if field.IsValid() {
-					values = append(values, field.Addr().Interface())
+					if field.Kind() == reflect.Slice {
+						var intr interface{}
+						slices[field] = &intr
+						values = append(values, &intr)
+					} else {
+						values = append(values, field.Addr().Interface())
+					}
 				} else {
 					var ignore interface{}
 					values = append(values, &ignore)
@@ -62,6 +71,29 @@ func Query(scope *Scope) {
 			}
 			scope.Err(rows.Scan(values...))
 
+			// TODO: PJW This should be done in postgres.go!!!
+			for k, v := range slices {
+				// should probably switch on source type as that is what drives this difference.  Integer type arrays
+				// seem to come back as arrays; string arrays come back as a postgres type string "{abc, def, ghi}", which
+				// must be parsed by pg/arrays.  This string is represented as a byte array.
+				switch k.Type().Elem().Kind() {
+				case reflect.String:
+					arrays.Unmarshal((*v).([]byte), k.Addr().Interface())
+				case reflect.Int, reflect.Int8, reflect.Int64:
+					if st := reflect.TypeOf(*v); st.Kind() == reflect.Slice {
+						sv := reflect.ValueOf(*v)
+						switch st.Elem().Kind() {
+						case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+							reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+							l := sv.Len()
+							k.Set(reflect.MakeSlice(k.Type(), l, l))
+							for i := 0; i < l; i++ {
+								k.Index(i).SetInt(sv.Index(i).Int())
+							}
+						}
+					}
+				}
+			}
 			if isSlice {
 				if isPtr {
 					dest.Set(reflect.Append(dest, elem.Addr()))
